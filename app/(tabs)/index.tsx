297ad74618982ebb31bb9605cb629cba66487343
RJ Villanueva <rj.villanueva89@gmail.com>
2024-10-31 18:54:13 +0800

    added nativewind

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,39 +1,21 @@
 import React from "react"
-import { ImageBackground, StyleSheet, Text, View } from "react-native"
+import { ImageBackground, Text, View } from "react-native"
 
 const HomeScreen = () => {
   return (
-    <View style={styles.container}>
+    <View className="flex grow flex-col">
       <ImageBackground
         source={require("@/assets/images/iced-coffee.png")}
         resizeMode="cover"
-        style={styles.image}
+        className="size-full flex justify-center"
       >
-        <Text style={styles.text}>Coffee Shop</Text>
+        <Text className="text-white text-4xl font-bold py-4 bg-black/50 text-center">
+          Coffee Shop
+        </Text>
+        <Text className="text-white">Explore</Text>
       </ImageBackground>
     </View>
   )
 }
 
 export default HomeScreen
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-  },
-  image: {
-    width: "100%",
-    height: "100%",
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
-  },
-  text: {
-    color: "white",
-    fontSize: 42,
-    fontWeight: "bold",
-    textAlign: "center",
-    backgroundColor: "rgba(0,0,0,0.5)",
-  },
-})
